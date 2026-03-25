2c2835291ef1bc702a7797de40a212afab2b66b6
ayastaga <ayavasu@gmail.com>
2026-03-25 10:21:08 -0400

    deleted neutramart_ui and renamed to frontend

--- neutramart_ui/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "./contexts/AuthContext";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import "./index.css";
-
-export default function App() {
-  const { user } = useAuth();
-  return user ? <Home /> : <Login />;
-}
