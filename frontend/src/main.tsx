24a1ab2544c1281771edc90639c6bf46b24a246f
ayastaga <ayavasu@gmail.com>
2026-03-26 01:22:08 -0400

    fixed scroll behavior when navigating on and to/fro pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./contexts/AuthContext.js";
 import { ReactLenis } from "lenis/react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 import App from "./App.js";
 import "./index.css";
 
@@ -12,10 +15,13 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <GoogleOAuthProvider clientId={clientId}>
         <AuthProvider>
           <ReactLenis root>
+            <Navbar />
             <App />
+            <Footer />
           </ReactLenis>
         </AuthProvider>
       </GoogleOAuthProvider>
