7f6e2cf0e4024975c1ae6cf57ed735d3cf596f68
ayastaga <ayavasu@gmail.com>
2026-03-25 23:51:59 -0400

    created home page with hero section; more to come in the next few commits

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,220 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { LayoutGroup, motion } from "framer-motion";
+import { TextRotate } from "@/components/ui/text-rotate";
+import Floating, { FloatingElement } from "@/components/ui/parallax-floating";
+
+const exampleImages = [
+  {
+    url: "https://i.pinimg.com/1200x/1e/04/08/1e04088f111c6bfbaef8871a63b58ce2.jpg",
+    author: "Pinterest #1",
+    title: "Ramen Noodle",
+  },
+  {
+    url: "https://i.pinimg.com/1200x/da/f5/b9/daf5b906f2105c7c51ed6c81e9c3bf67.jpg",
+    link: "https://i.pinimg.com/1200x/ef/83/58/ef83581cc50e65c3a26632213bcd2f70.jpg",
+    title: "Neon Palm",
+    author: "Tim Mossholder",
+  },
+  {
+    url: "https://i.pinimg.com/736x/79/06/c7/7906c7e923494a16b4ef3920bb3415ad.jpg",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-a-crowd-of-people-UgbxzloNGsc",
+    author: "ANDRII SOLOK",
+    title: "A blurry photo of a crowd of people",
+  },
+  {
+    url: "https://i.pinimg.com/736x/b3/26/76/b326764affba393de7afc50ca0f9581c.jpg",
+    link: "https://unsplash.com/photos/rippling-crystal-blue-water-9-OCsKoyQlk",
+    author: "Wesley Tingey",
+    title: "Rippling Crystal Blue Water",
+  },
+  {
+    url: "https://i.pinimg.com/1200x/3e/3b/0a/3e3b0a87555534593e0717cc1060b7ef.jpg",
+    link: "https://unsplash.com/de/fotos/mann-im-schwarzen-hemd-unter-blauem-himmel-m8RDNiuEXro",
+    author: "Serhii Tyaglovsky",
+    title: "Mann im schwarzen Hemd unter blauem Himmel",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1689553079282-45df1b35741b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-woman-with-a-flower-crown-on-her-head-0S3muIttbsY",
+    author: "Vladimir Yelizarov",
+    title: "A women with a flower crown on her head",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1721968317938-cf8c60fccd1a?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "A blurry photo of white flowers in a field",
+    author: "Eugene Golovesov",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-white-flowers-in-a-field-6qbx0lzGPyc",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1677338354108-223e807fb1bd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "Mathilde Langevin",
+    link: "https://unsplash.com/photos/a-table-topped-with-two-wine-glasses-and-plates-Ig0gRAHspV0",
+    title: "A table topped with two wine glasses and plates",
+  },
+];
+
+export default function Home() {
+  return (
+    <section className="w-full h-screen overflow-hidden md:overflow-visible flex flex-col items-center justify-center relative">
+      <Floating sensitivity={-0.5} className="h-full">
+        {/* small salad image */}
+        <FloatingElement
+          depth={0.5}
+          className="top-[15%] left-[2%] md:top-[25%] md:left-[5%]"
+        >
+          <motion.img
+            src={exampleImages[0].url}
+            alt={exampleImages[0].title}
+            className="w-0 h-0 lg:w-32 lg:h-24 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform -rotate-[3deg] shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          />
+        </FloatingElement>
+
+        {/* egg salad (top left) */}
+        <FloatingElement
+          depth={1}
+          className="top-[0%] left-[8%] md:top-[6%] md:left-[7%]"
+        >
+          <motion.img
+            src={exampleImages[1].url}
+            alt={exampleImages[1].title}
+            className="w-0 h-0 lg:w-60 lg:h-48 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform -rotate-12 shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.7 }}
+          />
+        </FloatingElement>
+
+        {/* man cooking (bottom left) */}
+        <FloatingElement
+          depth={4}
+          className="top-[90%] left-[6%] md:top-[60%] md:left-[8%]"
+        >
+          <motion.img
+            src={exampleImages[2].url}
+            alt={exampleImages[2].title}
+            className="w-0 h-0 lg:w-64 lg:h-64 object-cover -rotate-[4deg] hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.9 }}
+          />
+        </FloatingElement>
+
+        {/* kitchen food (top right) */}
+        <FloatingElement
+          depth={2}
+          className="top-[0%] left-[87%] md:top-[2%] md:left-[83%]"
+        >
+          <motion.img
+            src={exampleImages[3].url}
+            alt={exampleImages[3].title}
+            className="w-0 h-0 lg:h-56 lg:w-64 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rotate-[6deg] rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.1 }}
+          />
+        </FloatingElement>
+
+        {/* banana cream bowl (bottom right) */}
+        <FloatingElement
+          depth={1}
+          className="top-[78%] left-[83%] md:top-[48%] md:left-[80%]"
+        >
+          <motion.img
+            src={exampleImages[4].url}
+            alt={exampleImages[4].title}
+            className="w-0 h-0 lg:w-80 lg:h-80 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rotate-[19deg] rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.3 }}
+          />
+        </FloatingElement>
+      </Floating>
+
+      <div className="font-medium flex flex-col justify-center items-center w-[80%] z-50 pointer-events-auto">
+        <motion.h1
+          className="text-5xl sm:text-6xl md:text-7xl lg:text-6xl xl:text-7xl text-center w-full justify-center items-center flex-col flex whitespace-pre leading-tight font-calendas tracking-tight space-y-1 md:space-y-1"
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 0.3 }}
+        >
+          <span>
+            with <span className="text-green-600">NutraSmart</span>,
+          </span>
+          <span>counting calories </span>
+          <LayoutGroup>
+            <motion.span layout className="flex whitespace-pre">
+              <motion.span
+                layout
+                className="flex whitespace-pre"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              >
+                just got{" "}
+              </motion.span>
+              <TextRotate
+                texts={["simpler", "easier", "sexier", "smarter", "accurate"]}
+                mainClassName="overflow-hidden pr-3 text-green-700 py-0 pb-2 md:pb-4 rounded-xl"
+                staggerDuration={0.03}
+                staggerFrom="last"
+                rotationInterval={3000}
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              />
+            </motion.span>
+          </LayoutGroup>
+        </motion.h1>
+        <motion.p
+          className="text-sm sm:text-xl md:text-md xl:text-2xl lg:text-lg pt-4 sm:pt-8 md:pt-5 lg:pt-3"
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 0.5 }}
+        >
+          Just take a photo, and let our app do the work for you
+        </motion.p>
+
+        <div className="flex flex-row justify-center space-x-4 items-center mt-10 sm:mt-16 md:mt-10 lg:mt-9 text-xs">
+          <motion.button
+            className="text-lg sm:text-base md:text-lg lg:text-xl font-semibold tracking-tight text-background bg-foreground px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 lg:px-8 lg:py-3 rounded-full z-20 shadow-2xl font-calendas"
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            transition={{
+              duration: 0.2,
+              ease: "easeOut",
+              delay: 0.7,
+              scale: { duration: 0.2 },
+            }}
+            whileHover={{
+              scale: 1.05,
+              transition: { type: "spring", damping: 30, stiffness: 400 },
+            }}
+          >
+            <Link to="/login">
+              Get healthy <span className="font-serif ml-1">→</span>
+            </Link>
+          </motion.button>
+          <motion.button
+            className="text-lg sm:text-base md:text-lg lg:text-xl font-semibold tracking-tight text-white bg-gray-700 px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 lg:px-8 lg:py-3 rounded-full z-20 shadow-2xl font-calendas"
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            transition={{
+              duration: 0.2,
+              ease: "easeOut",
+              delay: 0.7,
+              scale: { duration: 0.2 },
+            }}
+            whileHover={{
+              scale: 1.05,
+              transition: { type: "spring", damping: 30, stiffness: 400 },
+            }}
+          >
+            <Link to="https://sylphiaconsulting.com/">Our website</Link>
+          </motion.button>
+        </div>
+      </div>
+    </section>
+  );
+}
